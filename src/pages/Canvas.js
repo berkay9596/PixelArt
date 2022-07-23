b6e4aa02bc3cd9baf7ac1ff27880112eec7ad405
Berkay <berkaayyuksel@gmail.com>
2022-07-24 02:04:22 +0300

    changes

--- src/pages/Canvas.js
+++ src/pages/Canvas.js
@@ -333,25 +333,31 @@ function Canvas() {
         </div>
         <article style={{lineHeight:"2rem"}} className="prose mx-auto container">
           <h2> Collaborative pixel painting</h2>
-          <p style={{padding: "0 1.5rem 0 1.5rem"}}>
+          <p 
+          style={{padding: "0 1.5rem 0 1.5rem"}}
+          >
             DESOPIXELART is the first ever collectible, collaborative pixel
             artwork to-be-created by the Deso community. Each canvas is 25x25
             pixels in size and has multiple authors, who create a unique piece
             of art by collaborating together. The completed artwork will be put
-            up for auction and when sold, auction proceeds will be distributed
+            up for an auction and when sold, auction proceeds will be distributed
             among all contributors evenly, depending on how many pixels they
             contributed to the canvas.
           </p>
 
           <h3>Earn by contributing</h3>
-          <p style={{padding: "0 1.5rem 0 1.5rem"}}>
+          <p
+           style={{padding: "0 1.5rem 0 1.5rem"}}
+           >
             By contributing to the artwork you are not only signing your pixels
             to the system. You are also entitled to a percentage of the proceeds
             from the auction of the final piece as well as a portion of every
             sale afterwards.
           </p>
 
-          <p style={{padding: "0 1.5rem 0 1.5rem"}}>
+          <p 
+          style={{padding: "0 1.5rem 0 1.5rem"}}
+          >
             When the completed canvas is auctioned: 85% of auction proceeds will
             be distributed amongst contributors. Every sale of the canvas
             afterwards: 5% of sale proceeds will be distributed amongst
